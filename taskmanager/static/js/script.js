c358694d08d9b5da848e32d114e917e4be6cc08f
Arthur Vilela <arthur.vilela@gmail.com>
2024-11-25 18:47:05 +0000

    Add form to create task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,11 +1,19 @@
-
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   // sidenav initialization
-  let sidenav = document.querySelectorAll('.sidenav');
+  let sidenav = document.querySelectorAll(".sidenav");
   M.Sidenav.init(sidenav);
-});
 
-document.addEventListener('DOMContentLoaded', function () {
+  // datepicker initialization
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: { done: "Select" }
+  });
+
+  // select initialization
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+
   // modal trigger
   var elems = document.querySelectorAll('.modal');
   var instances = M.Modal.init(elems);
