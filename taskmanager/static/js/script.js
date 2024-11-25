45c0dffc2401d39c811b6aa2c1d4f11376463a10
Arthur Vilela <arthur.vilela@gmail.com>
2024-11-25 17:27:52 +0000

    Add route to delete categories, modal to confirm deletion

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,6 +1,12 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  // modal trigger
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
+});
